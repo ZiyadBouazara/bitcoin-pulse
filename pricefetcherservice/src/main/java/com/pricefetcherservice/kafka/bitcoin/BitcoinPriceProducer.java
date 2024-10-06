47d5ab0cfee66b0cd7ea18f0c5e919e29a3a89fa
ZiyadBouazara <ziyad.bouazara.1@ulaval.ca>
2024-10-05 23:37:32 -0400

    cleaning

--- pricefetcherservice/src/main/java/com/pricefetcherservice/kafka/bitcoin/BitcoinPriceProducer.java
+++ pricefetcherservice/src/main/java/com/pricefetcherservice/kafka/bitcoin/BitcoinPriceProducer.java
@@ -5,7 +5,6 @@ import com.pricefetcherservice.kafka.PriceProducer;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
