3feb7fe9e1c702e8d7b433b41755fb5d920a4cd6
ZiyadBouazara <ziyad.bouazara.1@ulaval.ca>
2024-11-10 18:55:52 -0500

    cleaning: deleted legacy stockservice in java

--- stockservice/src/main/java/com/stockservice/infrastructure/kafka/PriceConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.stockservice.infrastructure.kafka;
-
-import com.stockservice.infrastructure.models.PriceEventModel;
-
-public interface PriceConsumer {
-    void consume(PriceEventModel priceEventModel);
-}
