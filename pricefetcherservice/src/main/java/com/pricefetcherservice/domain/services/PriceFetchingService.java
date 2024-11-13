3d9cf808accca57a0285719bd866f772e87161e9
ZiyadBouazara <ziyad.bouazara.1@ulaval.ca>
2024-11-12 20:09:13 -0500

    fix: PriceFetchingService now uses interface PriceWebSocketClient instead of implementation

--- pricefetcherservice/src/main/java/com/pricefetcherservice/domain/services/PriceFetchingService.java
+++ pricefetcherservice/src/main/java/com/pricefetcherservice/domain/services/PriceFetchingService.java
@@ -6,10 +6,8 @@ import com.pricefetcherservice.domain.models.PriceEvent;
 import com.pricefetcherservice.domain.PriceProducer;
 import com.pricefetcherservice.domain.PriceWebSocketClient;
 import com.pricefetcherservice.domain.models.StockSymbols;
-import com.pricefetcherservice.infrastructure.websocket.CoinbaseWebSocketClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -22,22 +20,22 @@ public class PriceFetchingService {
     private static final Logger logger = LoggerFactory.getLogger(PriceFetchingService.class);
     private static final List<StockSymbols> SUPPORTED_STOCKS = new ArrayList<>(List.of(StockSymbols.BTC_USD));
     private final PriceProducer bitcoinPriceProducer;
-    private final PriceWebSocketClient coinbaseWebSocketClient;
+    private final PriceWebSocketClient priceWebSocketClient;
 
-    public PriceFetchingService(PriceProducer bitcoinPriceProducer, CoinbaseWebSocketClient coinbaseWebSocketClient) {
+    public PriceFetchingService(PriceProducer bitcoinPriceProducer, PriceWebSocketClient priceWebSocketClient) {
         this.bitcoinPriceProducer = bitcoinPriceProducer;
-        this.coinbaseWebSocketClient = coinbaseWebSocketClient;
+        this.priceWebSocketClient = priceWebSocketClient;
     }
 
-    @Async
     @EventListener(ApplicationReadyEvent.class)
     public void startFetching() {
         try {
             logger.info("PriceFetchingService has started fetching bitcoin prices");
-            coinbaseWebSocketClient.setPriceUpdateListener(this::handlePriceUpdate);
-            coinbaseWebSocketClient.connect(SUPPORTED_STOCKS.stream().map(StockSymbols::toString).toList());
+            priceWebSocketClient.setPriceUpdateListener(this::handlePriceUpdate);
+            priceWebSocketClient.connect(SUPPORTED_STOCKS.stream().map(StockSymbols::toString).toList());
         } catch (Exception e) {
-            logger.error("Exception in startFetching(): " + e.getMessage());
+            logger.error("Failed to start fetching prices " + e.getMessage());
+            throw e;
         }
     }
 
@@ -46,7 +44,7 @@ public class PriceFetchingService {
             PriceEvent priceEvent = new ObjectMapper().readValue(priceUpdate, PriceEvent.class);
             bitcoinPriceProducer.sendPrice(priceEvent);
         } catch (JsonProcessingException e) {
-            logger.error("Json Parsing Error: " + e.getMessage());
+            logger.error("Failed to parse price update: {}", priceUpdate, e);
         }
     }
 }
