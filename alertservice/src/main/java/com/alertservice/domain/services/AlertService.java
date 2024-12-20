38bdbf79e16f0b34002ae15e3342c286cca5456e
Samiusss <sasab21@ulaval.ca>
2024-12-19 21:26:03 -0500

    test alert service

--- alertservice/src/main/java/com/alertservice/domain/services/AlertService.java
+++ alertservice/src/main/java/com/alertservice/domain/services/AlertService.java
@@ -9,22 +9,23 @@ import java.util.List;
 
 @Service
 public class AlertService {
-    private final PriceFilter priceFilter;
     private final AlertRepository alertRepository;
     private final AlertFactory alertFactory;
     private final AlertSender alertSender;
 
-    public AlertService(PriceFilter priceFilter, AlertRepository alertRepository, AlertFactory alertFactory,
+    public AlertService(AlertRepository alertRepository,
+                        AlertFactory alertFactory,
                         AlertSender alertSender) {
-        this.priceFilter = priceFilter;
         this.alertFactory = alertFactory;
         this.alertRepository = alertRepository;
         this.alertSender = alertSender;
     }
 
-    public void createAlert(BtcPrice price, BigDecimal priceDifference, String email, String phoneNumber) {
+    public Alert createAlert(BtcPrice price, BigDecimal priceDifference, String email, String phoneNumber) {
         Alert alert = alertFactory.createPriceAlert(price, priceDifference);
         alertRepository.saveAlert(alert);
+
+        return alert;
     }
 
     public void updateAlert(Long id, BtcPrice newPrice, BigDecimal newPriceDifference) {
--- alertservice/src/test/java/com/alertservice/AlertserviceApplicationTests.java
+++ alertservice/src/test/java/com/alertservice/AlertserviceApplicationTests.java
@@ -1,13 +1,47 @@
+/*
 package com.alertservice;
 
+import com.alertservice.domain.models.Alert;
+import com.alertservice.domain.models.BtcPrice;
+import com.alertservice.domain.services.AlertSender;
+import com.alertservice.domain.services.AlertService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static jdk.internal.org.objectweb.asm.util.CheckClassAdapter.verify;
+import static jdk.jfr.internal.jfc.model.Constraint.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AlertserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    public static final BtcPrice PRICE = new BtcPrice(BigDecimal.valueOf(10000));
+    public static final BigDecimal PRICE_DIFFERENCE = BigDecimal.valueOf(100);
+    public static final String EMAIL = "email@email.com";
+    public static final String PHONENUMBER = "1234567890";
+    @Mock
+    AlertSender alertSender;
+
+    @BeforeEach
+    void setUp() {
     }
 
+    @InjectMocks
+    AlertService alertService;
+
+    @Test
+    givenCorrectParams_whenCreateAlert_thenAlertIsCreated() {
+        Alert expectedAlert = new Alert(, , EMAIL, PHONENUMBER);
+        when(alertRepository.saveAlert(any(Alert.class))).thenReturn(expectedAlert);
+
+        alertService.createAlert(PRICE, PRICE_DIFFERENCE, EMAIL, PHONENUMBER);
+        verify(alertRepository, times(1)).saveAlert(any(Alert.class));
+    }
 }
+*/
